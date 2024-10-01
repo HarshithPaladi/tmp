bd5b446c728aab4a30f4b349f05c83e6ce936775
Harshith Paladi <hpaladi@presidio.com>
2024-10-01 15:00:51 +0530

    feat - Added more stocks

--- StockMarket/src/store/data.js
+++ StockMarket/src/store/data.js
@@ -1,29 +1,45 @@
-/**
- * @file data.js
- * @description This file contains a list of stock data used in the Stock Market application.
- */
+// Exporting a constant array named STOCK_LIST which contains a list of stock objects
+// Each stock object represents a company with its unique id, name, and current stock price
 
-/**
- * @constant {Array<Object>} STOCK_LIST
- * @description An array of stock objects, each representing a stock with its unique id, name, and current price.
- * @property {number} id - The unique identifier for the stock.
- * @property {string} name - The name of the company.
- * @property {number} price - The current price of the stock.
- */
 export const STOCK_LIST = [
     {
-        id: 1,
-        name: 'Apple',
-        price: 145.83,
+        id: 1, // Unique identifier for the stock
+        name: 'Apple', // Name of the company
+        price: 145.83, // Current stock price of the company
     },
     {
-        id: 2,
-        name: 'Microsoft Corporation',
-        price: 277.65,
+        id: 2, // Unique identifier for the stock
+        name: 'Microsoft Corporation', // Name of the company
+        price: 277.65, // Current stock price of the company
     },
     {
-        id: 3,
-        name: 'Amazon.com Inc.',
-        price: 3372.20,
+        id: 3, // Unique identifier for the stock
+        name: 'Amazon.com Inc.', // Name of the company
+        price: 3372.20, // Current stock price of the company
+    },
+    {
+        id: 4, // Unique identifier for the stock
+        name: 'Alphabet Inc.', // Name of the company
+        price: 2738.27, // Current stock price of the company
+    },
+    {
+        id: 5, // Unique identifier for the stock
+        name: 'Facebook, Inc.', // Name of the company
+        price: 355.64, // Current stock price of the company
+    },
+    {
+        id: 6, // Unique identifier for the stock
+        name: 'Tesla, Inc.', // Name of the company
+        price: 688.99, // Current stock price of the company
+    },
+    {
+        id: 7, // Unique identifier for the stock
+        name: 'NVIDIA Corporation', // Name of the company
+        price: 195.55, // Current stock price of the company
+    },
+    {
+        id: 8, // Unique identifier for the stock
+        name: 'Netflix, Inc.', // Name of the company
+        price: 533.84, // Current stock price of the company
     }
-];
+]
